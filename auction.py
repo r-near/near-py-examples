72b153624c4121dc33c206b2e24fe132732dc1be
r-near <163825889+r-near@users.noreply.github.com>
2025-03-06 22:47:39 -0800

    Update auction.py

--- auction.py
+++ auction.py
@@ -5,7 +5,7 @@ class Auction:
     def __init__(self):
         # Initialize state variables
         self.auction_end = 0  # Time when auction ends
-        self.highest_bid = \{
+        self.highest_bid = {
             "account_id": "",
             "amount": 0
         }
@@ -19,7 +19,7 @@ class Auction:
         """
         current_time_ms = time.time() * 1000
         self.auction_end = current_time_ms + (duration_minutes * 60 * 1000)
-        return \{"auction_end": self.auction_end}
+        return {"auction_end": self.auction_end}
     
     @call
     def place_bid(self):
