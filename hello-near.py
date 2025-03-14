1b3766495731b46be25a07bcd5e2c987177c07d3
r-near <163825889+r-near@users.noreply.github.com>
2025-03-14 13:39:59 -0700

    Update hello-near.py

--- hello-near.py
+++ hello-near.py
@@ -1,19 +1,20 @@
 # Import the necessary components from the NEAR SDK
-from near_sdk_py import view, call, init, Storage
+from near_sdk_py import view, call, Contract
 
 # Define contract class
 class HelloNear:
-    def __init__(self):
+    @init
+    def new(self):
         # Initialize state with default greeting
-        self.greeting = "Hello"
+        self.storage["greeting"] = "Hello"
     
     @view
     def get_greeting(self):
         """Returns the current greeting"""
-        return self.greeting
+        return self.storage["greeting"]
     
     @call
     def set_greeting(self, message):
         """Sets a new greeting"""
-        self.greeting = message
-        return self.greeting
+        self.storage["greeting"] = message
+        return message
